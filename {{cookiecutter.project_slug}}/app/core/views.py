9708e05d1c6f5f320e862028aea5686cfb7ac976
Rogério de Lima Sousa <rogerio.lima@ifpi.edu.br>
2024-06-07 12:41:24 -0300

    ajustes em arquivos estaticos

--- {{cookiecutter.project_slug}}/app/core/views.py
+++ {{cookiecutter.project_slug}}/app/core/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def hello(request):
-    return HttpResponse("Hello world!")
+    return render(request, 'home.html')
--- {{cookiecutter.project_slug}}/app/{{cookiecutter.project_slug}}/settings.py
+++ {{cookiecutter.project_slug}}/app/{{cookiecutter.project_slug}}/settings.py
@@ -123,10 +123,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR, "deploy/static")
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "deploy/media")
 MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "mediafiles"
 
 
 # Default primary key field type
